a486a73a0e2e9217265a68311dc151784f8356c3
introvertkid <phvu1403@gmail.com>
2024-09-08 15:05:05 +0700

    restyled UI

--- src/application/main/Main.java
+++ src/application/main/Main.java
@@ -1,36 +1,30 @@
 package main;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class Main extends Application {
+public class Main extends Application
+{
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage)
+    {
         try {
-            // Tải giao diện từ file main.fxml
-            Parent root = FXMLLoader.load(getClass().getResource("/FXML/BaseScene.fxml"));
-
-            // Tạo Scene và thêm giao diện vào Scene
-            Scene scene = new Scene(root, 1100, 650);
-
-            // Thiết lập Stage (cửa sổ)
+            Parent root = FXMLLoader.load(this.getClass().getResource("/FXML/BaseScene.fxml"));
+            Scene scene = new Scene(root, 1000.0, 620.0);
             primaryStage.setTitle("EVdictionary");
             primaryStage.setScene(scene);
-
-            // Hiển thị cửa sổ
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) {
-        // Khởi chạy ứng dụng JavaFX
         launch(args);
     }
 }
