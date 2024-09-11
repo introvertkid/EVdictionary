b2d0db0a09bf09e0cedef765690361a32df0ee05
MINH TRUONG <vutruongminh6d@gmail.com>
2024-09-11 10:45:43 +0700

    add removeFromCLI

--- src/application/main/OptionManagement.java
+++ src/application/main/OptionManagement.java
@@ -1,6 +1,14 @@
 package main;
-
+import java.io.IOException;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class OptionManagement
+
 {
     //Option 1: Translate word
     public static void translateWord()
@@ -25,4 +33,45 @@ public class OptionManagement
         System.out.println("Please press Enter to continue the program !");
         Reader.readLine();
     }
+    //Option 2: Remove words
+    public static void removeWords()
+    {
+
+        System.out.println("How many words you want to remove?");
+        //dung Reader class de doc file
+        Scanner ip=new Scanner(System.in);
+        int wordssize=ip.nextInt();
+        ArrayList<String> words=new ArrayList<>();
+        for(int i=0;i<wordssize;i++)
+        {
+            String line=ip.next();
+            words.add(line);
+        }
+        ip.close();
+        Path path=Path.of("dictionaries.txt");
+        try(Stream<String> lines=Files.lines(path))
+        {
+            String filteredContent = lines
+                    .filter(line -> {
+                        String[] parts = line.split("\t");
+                        return !words.contains(parts[0]);
+                    })
+                    .collect(Collectors.joining(System.lineSeparator()));
+            try(FileWriter fw=new FileWriter(path.toFile()))
+            {
+                fw.write(filteredContent);
+
+            }
+            catch(IOException ex)
+            {
+                System.out.println("Can't rewrite!!!");
+            }
+
+        }
+        catch(IOException ex)
+        {
+            System.out.println("Can't open file or file don't have any content!!!");
+        }
+    }
+
 }
