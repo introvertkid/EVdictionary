a8420f8cd93a6530f31b56ff9c9527a71eb361ce
MinhQuanThach <thachminhquan110905@gmail.com>
2024-09-11 15:26:21 +0700

    add words to file from cli

--- src/application/main/OptionManagement.java
+++ src/application/main/OptionManagement.java
@@ -1,15 +1,44 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.List;
 import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Files;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.ArrayList;
 
 public class OptionManagement
 {
+    //read words from txt file
+    public static void readWordfromFile()
+    {
+        String filePath = "D:\\code_app\\intellij\\intel_demo\\java_demo2\\src\\Vocabulary.txt";
+        List<String> words = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                words.add(line);
+            }
+            Collections.sort(words);
+            for(int i=0; i < words.size(); i++){
+                String[] vocab = words.get(i).split("\t");
+                Word thisWord = new Word(vocab[0], vocab[1]);
+                Dictionary dictionary = new Dictionary();
+                dictionary.put(thisWord);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //Option 2: Remove words
     public static void removeWordsFromCLI()
     {
@@ -74,5 +103,33 @@ public class OptionManagement
     {
         System.out.println("Please press Enter to continue the program !");
         Reader.readLine();
+    }
+
+    //Option 2: Add word
+    public static void addWord()
+    {
+        System.out.println("Enter the number of words you want to add: ");
+        int numberOfWordsAdd = Reader.readInt();
+        Reader.readLine();
+        for(int i=0; i < numberOfWordsAdd; i++) {
+            System.out.println("Enter the words and their translation (separated by tab): ");
+            String input = Reader.readLine();
+            String[] userWord;
+            if(input.contains("\t")){
+                userWord = input.split("\t");
+            }else{
+                System.out.println("Invalid format. Please use tab to separate word and translation.");
+                i--; //Retry the current word
+                continue;
+            }
+
+            if(userWord.length == 2){
+                Word newWord = new Word(userWord[0], userWord[1]);
+                dictionary.add(newWord);
+            }else{
+                System.out.println("Invalid input. Please try again");
+                i--;
+            }
+        }
     }
 }
